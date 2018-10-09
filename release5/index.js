3c87da40386b0a093a3592570b2d2b5f35c6d8e4
daryagolubeva <darya.golubewa@gmail.com>
2018-10-09 11:02:03 +0300

    bug is fixed;

--- release5/index.js
+++ release5/index.js
@@ -9,23 +9,20 @@ window.onload = function() {
 	
 
 	button.addEventListener('click', function(event) {
-let inputRows = document.getElementById('table-rows').value
-	let inputColumns = document.getElementById('table-columns').value;
-	let rows = parseInt(inputRows);
-	let columns = parseInt(inputColumns);
-	let button = document.getElementById('button');
+		let inputRows = document.getElementById('table-rows').value
+		let inputColumns = document.getElementById('table-columns').value;
+		let rows = parseInt(inputRows);
+		let columns = parseInt(inputColumns);
+		let button = document.getElementById('button');
 
 		table1 = document.getElementById('myTable');
-		tr = document.createElement('tr');
-		td = document.createElement('td');
 
-		console.log(tr,td)
-		console.log(rows)
 		for(let j = 0; j < rows; j++) {
+			tr = document.createElement('tr');
 			table1.appendChild(tr);
 
 			for(let i = 0; i < columns; i++) {
-
+				td = document.createElement('td');
 				tr.appendChild(td);
 				td.innerText = 'Строка-' + (j+1) + 'Столбец-' + (i+1);
 			};
